820da926c556b45a58848abb7ac60e91372eee49
Vo Huy <huyvo@it-43039.employee.southern.edu>
2018-04-21 16:17:18 -0400

    addgreen and blue switches

--- ColorPicker/ColorPicker/ViewController.swift
+++ ColorPicker/ColorPicker/ViewController.swift
@@ -13,7 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        colorView.backgroundColor = .black
+        // set the color
+        updateColor()
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,11 +25,34 @@ class ViewController: UIViewController {
     @IBOutlet weak var colorView: UIView!
     
     @IBAction func switchChanged(_ sender: UISwitch) {
-        if sender.isOn {
-            colorView.backgroundColor = .red
-        } else {
-            colorView.backgroundColor = .black
+        // update the color
+        updateColor()
+    }
+    
+    @IBOutlet weak var redSwitch: UISwitch!
+    
+    @IBOutlet weak var greenSwitch: UISwitch!
+    
+    @IBOutlet weak var blueSwitch: UISwitch!
+    
+    // update the color
+    func updateColor() {
+        var red: CGFloat = 0
+        var green: CGFloat = 0
+        var blue: CGFloat = 0
+        if redSwitch.isOn {
+            red = 1
         }
+        if greenSwitch.isOn {
+            green = 1
+        }
+        if blueSwitch.isOn {
+            blue = 1
+        }
+        
+        let mixedColor = UIColor(red: red, green: green, blue: blue, alpha: 1)
+        colorView.backgroundColor = mixedColor
+        
     }
 }
 
