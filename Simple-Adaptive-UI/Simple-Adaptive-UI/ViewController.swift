a11fb2fc0a4ebed459b1165221ee814e31daf32d
Vo Huy <huyvo@it-43039.employee.southern.edu>
2018-04-22 14:36:00 -0400

    added Aminal Sounds project

--- Simple-Adaptive-UI/Simple-Adaptive-UI/ViewController.swift
+++ Simple-Adaptive-UI/Simple-Adaptive-UI/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController {
 
     let elementList = ["Carbon", "Gold", "Chlorine", "Sodium"]
+
     var currentElementIndex = 0
     
     override func viewDidLoad() {
