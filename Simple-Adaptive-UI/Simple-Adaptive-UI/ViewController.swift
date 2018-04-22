e82764ad01ed9334ff6ad73ac6da121f3ad5b3b0
Vo Huy <huyvo@it-43039.employee.southern.edu>
2018-04-22 12:37:00 -0400

    added outlets and actions + set launch screen

--- Simple-Adaptive-UI/Simple-Adaptive-UI/ViewController.swift
+++ Simple-Adaptive-UI/Simple-Adaptive-UI/ViewController.swift
@@ -10,9 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let elementList = ["Carbon", "Gold", "Chlorine", "Sodium"]
+    var currentElementIndex = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        updateElement()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +24,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var answerLabel: UILabel!
+    
+    @IBAction func showAnswer(_ sender: UIButton) {
+    }
+    
+    @IBAction func gotoNextElement(_ sender: UIButton) {
+        updateElement()
+    }
+    
+    func updateElement() {
+        answerLabel.text = "?"
+        let elementName = elementList[currentElementIndex]
+        let image = UIImage(named: elementName)
+        imageView.image = image
+    }
 }
 
