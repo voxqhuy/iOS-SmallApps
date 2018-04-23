158eda46aae8e646d7914657a09fd69fba2b7e72
Vo Huy <huyvo@it-43039.employee.southern.edu>
2018-04-22 20:27:34 -0400

    fixed the color

---
+++ Rock-Paper_Scissors/Rock-Paper_Scissors/Color.swift
@@ -0,0 +1,14 @@
+//
+//  Color.swift
+//  Rock-Paper_Scissors
+//
+//  Created by Vo Huy on 4/22/18.
+//  Copyright © 2018 Vo Huy. All rights reserved.
+//
+
+import Foundation
+
+class Color {
+    static let lightRed = UIColor(red: 237.0, green: 131.0, blue: 124.0, alpha: 1)
+    case lightGreen = UIColor(red: 177.0/255.0, green: 220.0/255.0, blue: 151.0/255.0, alpha: 1)
+}
