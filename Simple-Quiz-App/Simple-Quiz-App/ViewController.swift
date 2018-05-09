bb7a384a92c2c3523f4c650f10f987c92f96f7ab
Vo Huy <huyvo@it-43039.employee.southern.edu>
2018-05-09 15:04:35 -0400

    finished basic modal

--- Simple-Quiz-App/Simple-Quiz-App/ViewController.swift
+++ Simple-Quiz-App/Simple-Quiz-App/ViewController.swift
@@ -12,11 +12,42 @@ class ViewController: UIViewController {
     @IBOutlet var questionLabel: UILabel!
     @IBOutlet var answerLabel: UILabel!
     
+    // MARK: Properties
+    // An array holding questions
+    let questions: [String] = [
+        "What is 7 + 7?",
+        "What is the capitak if Vermont?",
+        "What is cognac made from?"
+    ]
+    
+    // An array holding answers
+    let answers: [String] = [
+        "14",
+        "Montpelier",
+        "Grapes"
+    ]
+    
+    // the current question index
+    var currentQuestionIndex: Int = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        questionLabel.text = questions[currentQuestionIndex]
+    }
+    
+    // MARK: Private methods
     @IBAction func showNextQuestion(_ sender: UIButton) {
+        // reset question back to the first one when it is the end of the question bank, else keep moving on
+        currentQuestionIndex = (currentQuestionIndex == questions.count - 1) ? 0 : (currentQuestionIndex + 1)
         
+        let question = questions[currentQuestionIndex]
+        questionLabel.text = question
+        answerLabel.text = "???"
     }
     
     @IBAction func showAnswer(_ sender: UIButton) {
+        let answer = answers[currentQuestionIndex]
+        answerLabel.text = answer
         
     }
 }
