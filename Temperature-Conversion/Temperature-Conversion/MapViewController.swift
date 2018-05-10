3a4dc9523e4f703807d936ea00d16c6c96c017e5
voxqhuy <voxqhuy@gmail.com>
2018-05-10 10:08:44 -0400

    added the map view programatically

--- Temperature-Conversion/Temperature-Conversion/MapViewController.swift
+++ Temperature-Conversion/Temperature-Conversion/MapViewController.swift
@@ -7,8 +7,36 @@
 //
 
 import UIKit
+import MapKit
 
 class MapViewController: UIViewController {
+    
+    var mapView: MKMapView!
+    
+    override func loadView() {
+        // Create a map view
+        mapView = MKMapView()
+        
+        // Set it as the view of this view controller
+        view = mapView
+        
+        // add a segmented control
+        let segmentedControl = UISegmentedControl(items: ["Standard", "Hybrid", "Satellite"])
+        segmentedControl.backgroundColor = UIColor.white.withAlphaComponent(0.5)
+        segmentedControl.selectedSegmentIndex = 0
+        segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(segmentedControl)
+        
+        // the map view's constraints
+        let topConstraint = segmentedControl.topAnchor.constraint(equalTo: view.topAnchor)
+        let leadingConstraint = segmentedControl.leadingAnchor.constraint(equalTo: view.leadingAnchor)
+        let trailingConstraint = segmentedControl.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        
+        topConstraint.isActive = true
+        leadingConstraint.isActive = true
+        trailingConstraint.isActive = true
+        
+    }
     
     override func viewDidLoad() {
         print("running map")
