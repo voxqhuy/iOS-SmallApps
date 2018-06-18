b86c8e7ac0f73f77a65cd0a6c32d815611726dcb
voxqhuy <voxqhuy@gmail.com>
2018-06-18 12:53:32 -0400

    added 7littlewords game for ipad

---
+++ 7LittleWords/7LittleWords/ViewController.swift
@@ -0,0 +1,100 @@
+//
+//  ViewController.swift
+//  7LittleWords
+//
+//  Created by Vo Huy on 6/18/18.
+//  Copyright © 2018 Vo Huy. All rights reserved.
+//
+
+import UIKit
+import GameplayKit
+
+class ViewController: UIViewController {
+
+    // MARK: Properties
+    var letterButtons = [UIButton]()
+    var activatedButtons = [UIButton]()
+    var solutions = [String]()
+    
+    var score = 0
+    var level = 1
+    
+    // MARK: Outlets
+    @IBOutlet var cluesLabel: UILabel!
+    @IBOutlet var answersLabel: UILabel!
+    @IBOutlet var scoreLabel: UILabel!
+    @IBOutlet var currentAnswer: UITextField!
+    
+    // MARK: Actions
+    @IBAction func submitTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func clearTapped(_ sender: UIButton) {
+    }
+    
+    // MARK: - View life cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // filter out to get the buttons only
+        for subview in view.subviews where subview.tag == 1001 {
+            let btn = subview as! UIButton
+            letterButtons.append(btn)
+            btn.addTarget(self, action: #selector(letterTapped), for: .touchUpInside)
+        }
+        
+        loadLevel()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+}
+
+extension ViewController {
+    
+    @objc func letterTapped(btn: UIButton) {
+        
+    }
+    
+    func loadLevel() {
+        var clueString = ""
+        var solutionString = ""
+        var letterBits = [String]()
+        
+        if let levelFilePath = Bundle.main.path(forResource: "level\(level)", ofType: "txt") {
+            if let levelContents = try? String(contentsOfFile: levelFilePath) {
+                var lines = levelContents.components(separatedBy: "\n")
+                lines = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: lines) as! [String]
+                for (index, line) in lines.enumerated() {
+                    let parts = line.components(separatedBy: ": ")
+                    let answer = parts[0]
+                    let clue = parts[1]
+                    
+                    clueString += "\(index + 1). \(clue)\n"
+                    
+                    let solutionWord = answer.replacingOccurrences(of: "|", with: "")
+                    solutionString += "\(solutionWord.count) letters\n"
+                    solutions.append(solutionWord)
+                    
+                    let bits = answer.components(separatedBy: "|")
+                    letterBits += bits
+                }
+            }
+        }
+        
+        cluesLabel.text = clueString.trimmingCharacters(in: .whitespacesAndNewlines)
+        answersLabel.text = solutionString.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        letterBits = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: letterBits) as! [String]
+        
+        if letterBits.count == letterButtons.count {
+            for i in 0 ..< letterBits.count {
+                letterButtons[i].setTitle(letterBits[i], for: .normal)
+            }
+        }
+    }
+}
