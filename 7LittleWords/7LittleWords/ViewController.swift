92bcc524b20a3afd99eb606ca037210d2068c278
voxqhuy <voxqhuy@gmail.com>
2018-06-18 18:01:16 -0400

    fnished the little words project

--- 7LittleWords/7LittleWords/ViewController.swift
+++ 7LittleWords/7LittleWords/ViewController.swift
@@ -16,7 +16,12 @@ class ViewController: UIViewController {
     var activatedButtons = [UIButton]()
     var solutions = [String]()
     
-    var score = 0
+    var attempts = 1
+    var score = 0 {
+        didSet {
+            scoreLabel.text = "Score: \(score)"
+        }
+    }
     var level = 1
     
     // MARK: Outlets
@@ -27,9 +32,27 @@ class ViewController: UIViewController {
     
     // MARK: Actions
     @IBAction func submitTapped(_ sender: UIButton) {
+        
+        if let solutionIndex = solutions.index(of: currentAnswer.text!){
+            score += 1
+            if score == 7 {
+                levelUp()
+            } else {
+                alertRightAnswer()
+                revealAnswer(atIndex: solutionIndex)
+            }
+            
+        } else {
+            reShowButtons()
+            alertWrongAnswer()
+        }
+        
+        resetStates()
     }
     
     @IBAction func clearTapped(_ sender: UIButton) {
+        reShowButtons()
+        resetStates()
     }
     
     // MARK: - View life cycle
@@ -54,12 +77,8 @@ class ViewController: UIViewController {
     
 }
 
+// MARK: - Layout
 extension ViewController {
-    
-    @objc func letterTapped(btn: UIButton) {
-        
-    }
-    
     func loadLevel() {
         var clueString = ""
         var solutionString = ""
@@ -96,5 +115,84 @@ extension ViewController {
                 letterButtons[i].setTitle(letterBits[i], for: .normal)
             }
         }
+        
+        for btn in letterButtons {
+            btn.isEnabled = true
+        }
+    }
+    
+    func reShowButtons() {
+        for btn in activatedButtons {
+            btn.isHidden = false
+        }
+    }
+    
+    func revealAnswer(atIndex index: Int) {
+        var splitClues = cluesLabel.text!.components(separatedBy: "\n")
+        splitClues[index] = "CLEARED"
+        cluesLabel.text = splitClues.joined(separator: "\n")
+        
+        var splitAnswers = answersLabel.text!.components(separatedBy: "\n")
+        splitAnswers[index] = currentAnswer.text!
+        answersLabel.text = splitAnswers.joined(separator: "\n")
+    }
+}
+
+// MARK: - User Interaction
+extension ViewController {
+    
+    @objc func letterTapped(btn: UIButton) {
+        print(#function)
+        if attempts == 5 {
+            alertLongWord()
+            return
+        }
+        
+        currentAnswer.text = currentAnswer.text! + btn.titleLabel!.text!
+        btn.isHidden = true
+        activatedButtons.append(btn)
+        
+        attempts += 1
+    }
+}
+
+// MARK: Helper methods
+extension ViewController {
+    
+    func alertRightAnswer() {
+        let alertVC = UIAlertController(title: "Congrats!!", message: "Correct answer", preferredStyle: .alert)
+        alertVC.addAction(UIAlertAction(title: "Okay", style: .default))
+        present(alertVC, animated: true)
+    }
+    
+    func alertWrongAnswer() {
+        let alertVC = UIAlertController(title: "Oopss!!", message: "Wrong answer", preferredStyle: .alert)
+        alertVC.addAction(UIAlertAction(title: "Okay", style: .default))
+        present(alertVC, animated: true)
+    }
+    
+    func alertLongWord() {
+        let alertVC = UIAlertController(title: "Too many letters", message: "You have used too many letters", preferredStyle: .alert)
+        alertVC.addAction(UIAlertAction(title: "Okay", style: .default))
+        present(alertVC, animated: true)
+    }
+    
+    func resetStates() {
+        currentAnswer.text = ""
+        // TODO: blur out the answer
+        activatedButtons.removeAll()
+        attempts = 1
+    }
+    
+    func levelUp() {
+        let alertVC = UIAlertController(title: "Level up!", message: "Congratulation, you finished all words for this level.", preferredStyle: .alert)
+        alertVC.addAction(UIAlertAction(title: "Okay", style: .default) {
+            action in
+            
+            self.level += 1
+            self.solutions.removeAll(keepingCapacity: true)
+            self.loadLevel()
+        })
+        present(alertVC, animated: true)
     }
 }
